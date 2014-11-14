e57238ace030647d50fcebdf224fd83294aa6ef8
John Felten <john_felten@yahoo.com>
2014-11-13 17:05:10 -0800

    add callback machanism, Fix response mapping add another example.

--- knowhow-shell.js
+++ knowhow-shell.js
@@ -1,10 +1,10 @@
 var pty = require('pty.js');
 var async = require('async');
+var process = require('process');
 var EventEmitter = require('events').EventEmitter;
 var eventEmitter = new EventEmitter();
 
-var retCodeRE = new RegExp("ret code:\d*");
-var executionDoneRE = new RegExp("D_O_N_E \d*");
+var retCodeRE = /ret code:\d+/;
 
 var jobsInProgress = {};
 
@@ -15,11 +15,13 @@ var cancelJob = function(job) {
 	}	
 };
 
-var executeJob = function(job) {
+var executeJob = function(job, callback) {
 	setEnv(job);
-	var scriptRuntime = {};	
-	scriptRuntime.currentStep=0;
-	scriptRuntime.progressStepLength = Math.floor(100/job.script.commands.length);
+	var scriptRuntime = {	
+		currentStep: 0,
+		output: '',
+		progressStepLength: Math.floor(100/job.script.commands.length)
+	};
 	
 	var term = pty.spawn('bash', [], {
 	  name: 'xterm-color',
@@ -32,34 +34,48 @@ var executeJob = function(job) {
 	console.log(term.process);
 	var currentCommand;	
 	term.on('data', function(data) {
-  	  //console.log(data);
-  	  scriptRuntime.currentCommand.output+=data;
-	  if (scriptRuntime.currentCommand.responses) {
-	  	for (response in scriptRuntime.currentCommand.responses) {
-	  		var re = new RegExp(response,"g");
-	  		if (data.match(response)) {
-	  			term.write(currentCommand.responses[response]+"\r");
-	  		}
-	  	}
-	  }
+		if (data) {
+	  	  //console.log(data);
+	  	  output=data.split(retCodeRE)[0];
+	  	  if (output) {
+	  	  	scriptRuntime.output+=output
+	  	  	scriptRuntime.currentCommand.output+=output;
+	  	  }
+		  if (scriptRuntime.currentCommand.responses) {
+		  	for (response in scriptRuntime.currentCommand.responses) {
+		  		var re = new RegExp(response,"g");
+		  		if (data.match(response)) {
+		  			//console.log("response: "+response+" "+scriptRuntime.currentCommand.responses[response]);
+		  			term.write(scriptRuntime.currentCommand.responses[response]+"\r");
+		  		}
+		  	}
+		  }
+		  
+		  if (data.match(retCodeRE)) {
+		  	
+		  	scriptRuntime.currentCommand.returnCode = data.match(retCodeRE)[0].split(":")[1];
+		  	//console.log('return code: '+scriptRuntime.currentCommand.returnCode);
+		  	if (scriptRuntime.currentCommand.returnCode != 0) {
+		  		eventEmitter.emit('execution-error',scriptRuntime.currentCommand);
+		  		scriptRuntime.currentCommand.callback(new Error(scriptRuntime.currentCommand.output));
+		  	} else {
+		  		eventEmitter.emit('execution-complete', scriptRuntime.currentCommand);
+		  		scriptRuntime.currentCommand.callback();
+		  	}
+		  	job.progress=scriptRuntime.currentStep*scriptRuntime.progressStepLength;
+		  	job.status = 'completed: '+scriptRuntime.currentCommand
+		  	eventEmitter.emit('job-update',{id: job.id, status: job.status, progress: job.progress});
+		  }
+		}
 	  
-	  if (data.match(retCodeRE)) {
-	  	scriptRuntime.currentCommand.returnCode = data.match(retCodeRE)[0].split(":")[1];
-	  }
-	  if (data.match(executionDoneRE)) {
-	  	console.log(scriptRuntime.currentCommand.command+": is done");
-	  	scriptRuntime.currentCommand.callback();
-	  	eventEmitter.emit('execution-complete', currentCommand);
-	  	job.progress=scriptRuntime.currentStep*scriptRuntime.progressStepLength;
-	  	job.status = 'completed: ;+scriptRuntime.currentCommand
-	  	//console.log("progress="+job.progress+" currnetStep: "+scriptRuntime.currentStep+" step length: "+scriptRuntime.progressStepLength);
-	    eventEmitter.emit('job-update',{id: job.id, status: job.status, progress: job.progress});
-	  } 
 	});
-	term.on('error', function(data) {
-		console.log(data);
+	term.on('error', function(err) {
+		//console.log(err.message);
 		clearInterval(progressCheck);
-		term.exit();
+		term.end();
+		if (callback) {
+			callback(err, scriptRuntime);
+		}
 	});
 	var progressCheck = setInterval(function() {
 	    job.progress++;
@@ -69,20 +85,23 @@ var executeJob = function(job) {
 	//console.log(job);
 	async.eachSeries(job.script.commands, function(command,callback) {
 		scriptRuntime.currentStep++;
-		console.log(command);
+		//console.log(command);
 		command.callback = callback;
+		command.output = '';
 		scriptRuntime.currentCommand = command;
-		term.write(command.command+' || echo "ret code:"$? && echo \"D_O_N_E\" $?\r');
+		term.write(command.command+'; echo "ret code:"$?\r');
 		//term.write("wait | echo \"D_O_N_E: "+currentCommand.command+'\r');
 	    }.bind({scriptRuntime: scriptRuntime}), 
 	    function(err) {
 	    	if (err) {
-
-				logger.error('job error' + err);
 				job.progress=0;
 				job.status=err.message;
 				eventEmitter.emit('job-error',job);
 				clearInterval(progressCheck);
+				term.end();
+				if (callback) {
+					callback(err, scriptRuntime);
+				}
 				return;
 			}
 			job.progress=0;
@@ -92,16 +111,37 @@ var executeJob = function(job) {
 			clearInterval(progressCheck);
 	        //logger.info("done");
 	    	term.end();
+	    	if (callback) {
+				callback(undefined, scriptRuntime);
+			}
+		}
 	    	
-	  	}
 	 );
 	 
 }
 
 var setEnv = function(job) {
- 	job.script.env["working_dir"]=job.working_dir;
-	
 
+	if (!job || !job.script) {
+		return;
+	}
+	if (!job.script.env) {
+		job.script.env = {};
+	}
+	if (job.working_dir) {
+ 		job.script.env["working_dir"]=job.working_dir;
+ 	} else {
+ 		job.script.env["working_dir"]='./';
+ 	}
+	
+	//add the process.env to job.env
+	for (envVar in process.env) {
+		if (!job.script.env[envVar]) {
+			job.script.env[envVar] = process.env[envVar];
+		}
+	}
+
+	
 	replaceVar = function(regEx,varName, searchString) {
 		    var iteration=0;
 		    var replacedString = searchString;
@@ -135,15 +175,15 @@ var setEnv = function(job) {
 		for (variable in job.script.env) {
 			job.script.env[variable] = replaceVar(dollarRE,variable,job.script.env[variable]);
 			job.script.env[variable] = replaceVar(dollarBracketRE,variable,job.script.env[variable]);
-			//console.log(variable+'='+env[variable])
+			//console.log(variable+'='+job.script.env[variable])
 		}
 		
 		for (commandIndex in job.script.commands) {
 			var command = job.script.commands[commandIndex];
 			for (response in command.responses) {
-				
-				command[response] =  replaceVar(dollarRE,variable,command[response]);
-				command[response] =  replaceVar(dollarBracketRE,variable,command[response]);
+				command.responses[response] =  replaceVar(dollarRE,variable,command.responses[response]);
+				command.responses[response] =  replaceVar(dollarBracketRE,variable,command.responses[response]);
+				//console.log('repsonse: '+response+'='+command.responses[response]);
 			}
 		}
 	
