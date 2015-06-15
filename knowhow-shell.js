13e750a5f5f041b2ba33273c521361ffcb33cad6
knowhow <john@zenzic.com>
2015-06-15 11:24:50 -0700

    correct execution-error event type

--- knowhow-shell.js
+++ knowhow-shell.js
@@ -131,7 +131,7 @@ var executeJobAsSubProcess = function(job, callback) {
 				delete job.subProcess;
 				//eventEmitter.emit("job-complete", data);
 				if (callback) callback(undefined, data);
-			} else if (eventType =='execution error' || eventType =='job-error' || eventType =='job-cancel' ){
+			} else if (eventType =='execution-error' || eventType =='job-error' || eventType =='job-cancel' ){
 				clearTimeout(job.timeout);
 				clearInterval(job.progressCheck);
 				delete job.timeout;
